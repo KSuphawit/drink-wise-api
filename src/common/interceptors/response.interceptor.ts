fd41dc7dac51e3f544407efa613411d19c1e541b
Ksuphawit <ksuphawit@Ksuphawits-MacBook-Pro.local>
2024-07-30 01:26:30 +0700

    feat: add response interceptor

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,25 @@
+import { BaseResponse } from '@/common/reponses/base.response';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class ResponseInterceptor<T>
+  implements NestInterceptor<T, BaseResponse<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<BaseResponse<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        const statusCode = context.switchToHttp().getResponse().statusCode;
+        return new BaseResponse<T>(statusCode, 'Success', data);
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { AppModule } from '@/app.module';
+import { ResponseInterceptor } from '@/common/interceptors/response.interceptor';
 import { AppConfiguration } from '@/config/app.configuration';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
@@ -7,6 +8,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   const appConfig = app.get(AppConfiguration);
   await app.listen(appConfig.port, appConfig.host);
 }
